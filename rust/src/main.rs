2196df5fb60306d7dba98909d87e6882d5b4e84e
Niklas Larsson <niklaslarsson95@gmail.com>
2016-05-31 18:25:24 +1000

    Fixed a bunch of compile errors
    Still stuck on one

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -29,12 +29,12 @@ const SCORE_EMPTY_WEIGHT:       f32 = 270.0;
 const ROW_MASK: u64 = 0xFFFF; 
 const COL_MASK: u64 = 0x000F000F000F000F;
 
-fn print_board(board: u64) {
+fn print_board(board: &mut u64) {
     for i in 0..4 {
         for j in 0..4 {
-            let power = board & 0xf; //Take the last byte in the number
+            let power = *board & 0xf; //Take the last byte in the number
             print!("{}", if power == 0 {0} else {2 << power}); //2<<power = 2^power
-            board >>= 4; //Next byte
+            *board >>= 4; //Next byte
         }
         print!("\n");
     }
@@ -61,17 +61,17 @@ fn transpose(board: u64) -> u64 {
     b1 | (b2 >> 24) | (b3 << 24)
 }
 
-fn count_empty(board: u64) -> u64 {
-    board |= (board >> 2) & 0x3333333333333333;
-    board |=  board >> 1;
-    board  = !board & 0x1111111111111111;
+fn count_empty(board: &mut u64) -> u64 {
+    *board |= (*board >> 2) & 0x3333333333333333;
+    *board |=  *board >> 1;
+    *board  = !*board & 0x1111111111111111;
 
-    board += board >> 32;
-    board += board >> 16;
-    board += board >>  8;
-    board += board >>  4;
+    *board += *board >> 32;
+    *board += *board >> 16;
+    *board += *board >>  8;
+    *board += *board >>  4;
     
-    board & 0xF as u64
+    *board & 0xF as u64
 }
 
 fn init_tables() {
@@ -83,7 +83,7 @@ fn init_tables() {
             (row >> 12) & 0xF
         ];
 
-        let score: f32 = 0.0;
+        let mut score: f32 = 0.0;
         for i in 0..4 {
             let rank = line[i];
             if rank >= 2 {
@@ -92,12 +92,12 @@ fn init_tables() {
         }
         score_table[row as usize] = score;
 
-        let sum: f32 = 0.0;
-        let empty = 0;
-        let merges = 0;
+        let mut sum: f32 = 0.0;
+        let mut empty = 0;
+        let mut merges = 0;
 
-        let prev = 0;
-        let counter = 0;
+        let mut prev = 0;
+        let mut counter = 0;
         for i in 0..4 {
             let rank = line[i];
             sum += (rank as f32).powf(SCORE_SUM_POWER);
@@ -117,8 +117,8 @@ fn init_tables() {
             merges += 1 + counter;
         }
 
-        let monotonicity_left : f32 = 0.0;
-        let monotonicity_right: f32 = 0.0;
+        let mut monotonicity_left : f32 = 0.0;
+        let mut monotonicity_right: f32 = 0.0;
         for i in 1..4 {
             if line[i-1] > line[i] {
                 monotonicity_left += (line[i-1] as f32).powf(SCORE_MONOTONICITY_POWER) - (line[i] as f32).powf(SCORE_MONOTONICITY_POWER);
@@ -134,9 +134,9 @@ fn init_tables() {
             SCORE_SUM_WEIGHT * sum as f32;
 
         //Exectute a move to the left
-        let i = 0;
+        let mut i = 0;
         while i < 3 {
-            let j = 0;
+            let mut j = 0;
             while j < 4 {
                 if line[j] != 0 {break};
                 j += 1;
@@ -171,7 +171,7 @@ fn init_tables() {
 }
 
 fn execute_move_0(board: u64) -> u64 {
-    let ret = board;
+    let mut ret = board;
     let t   = transpose(board);
     ret ^= col_up_table[((t >>  0) & ROW_MASK) as usize] << 0;
     ret ^= col_up_table[((t >> 16) & ROW_MASK) as usize] << 4;
@@ -181,7 +181,7 @@ fn execute_move_0(board: u64) -> u64 {
 }
 
 fn execute_move_1(board: u64) -> u64 {
-    let ret = board;
+    let mut ret = board;
     let t   = transpose(board);
     ret ^= col_down_table[((t >>  0) & ROW_MASK) as usize] << 0;
     ret ^= col_down_table[((t >> 16) & ROW_MASK) as usize] << 4;
@@ -191,7 +191,7 @@ fn execute_move_1(board: u64) -> u64 {
 }
 
 fn execute_move_2(board: u64) -> u64 {
-    let ret = board;
+    let mut ret = board;
     ret ^= (row_left_table[((board >>  0) & ROW_MASK) as usize] << 0) as u64;
     ret ^= (row_left_table[((board >> 16) & ROW_MASK) as usize] << 16) as u64;
     ret ^= (row_left_table[((board >> 32) & ROW_MASK) as usize] << 32) as u64;
@@ -200,7 +200,7 @@ fn execute_move_2(board: u64) -> u64 {
 }
 
 fn execute_move_3(board: u64) -> u64 {
-    let ret = board;
+    let mut ret = board;
     ret ^= (row_right_table[((board >>  0) & ROW_MASK) as usize] << 0) as u64;
     ret ^= (row_right_table[((board >> 16) & ROW_MASK) as usize] << 16) as u64;
     ret ^= (row_right_table[((board >> 32) & ROW_MASK) as usize] << 32) as u64;
@@ -218,8 +218,8 @@ fn execute_move(mv: u8, board: u64) -> u64 {
     }
 }
 
-fn get_max_rank(board: u64) -> u16 {
-    let maxrank: u16 = 0;
+fn get_max_rank(mut board: u64) -> u16 {
+    let mut maxrank: u16 = 0;
     while board != 0 {
         maxrank = max(maxrank, (board & 0xF) as u16);
         board >>= 4;
@@ -227,8 +227,8 @@ fn get_max_rank(board: u64) -> u16 {
     maxrank
 }
 
-fn count_distinct_tiles(board: u64) -> u16 {
-    let bitset: u16 = 0;
+fn count_distinct_tiles(mut board: u64) -> u16 {
+    let mut bitset: u16 = 0;
     while board != 0 {
         bitset |= 1 << (board & 0xF);
         board >>= 4;
@@ -236,7 +236,7 @@ fn count_distinct_tiles(board: u64) -> u16 {
 
     bitset >>= 1;
 
-    let count = 0;
+    let mut count = 0;
     while bitset != 0 {
         bitset &= bitset - 1;
         count+= 1;
@@ -265,15 +265,15 @@ fn score_board(board: u64)  -> f32 {
 const CPROB_THRESH_BASE: f32 = 0.0001; // Will not evaluate nodes less likely than this
 const CACHE_DEPTH_LIMIT: u32 = 15;
 
-fn score_move_node(state: &eval_state, board: u64, cprob: f32) -> f32 {
-    let best: f32 = 0.0;
+fn score_move_node(mut state: &mut eval_state, board: u64, cprob: f32) -> f32 {
+    let mut best: f32 = 0.0;
     state.curdepth+= 1;
     for mv in 0..4 {
         let newboard: u64 = execute_move(mv, board);
         state.moves_evaled+= 1;
 
         if board != newboard {
-            best = best.max(score_tilechoose_node(&state, newboard, cprob));
+            best = best.max(score_tilechoose_node(&mut state, newboard, cprob));
         }
     }
     state.curdepth -= 1;
@@ -281,7 +281,7 @@ fn score_move_node(state: &eval_state, board: u64, cprob: f32) -> f32 {
     best
 }
 
-fn score_tilechoose_node(state: &eval_state, board:u64, cprob:f32) -> f32 {
+fn score_tilechoose_node(mut state: &mut eval_state,mut board:u64,mut cprob:f32) -> f32 {
     if cprob < CPROB_THRESH_BASE || state.curdepth >= state.depth_limit {
         state.maxdepth = max(state.curdepth, state.maxdepth);
         return score_heur_board(board);
@@ -303,17 +303,17 @@ fn score_tilechoose_node(state: &eval_state, board:u64, cprob:f32) -> f32 {
         }
     }
 
-    let num_open = count_empty(board);
+    let num_open = count_empty(&mut board);
     cprob /= num_open as f32;
 
-    let res: f32 = 0.0;
-    let tmp = board;
-    let tile_2: u64 = 1;
+    let mut res: f32 = 0.0;
+    let mut tmp = board;
+    let mut tile_2: u64 = 1;
 
     while tile_2 != 0 {
         if (tmp & 0xF) == 0 {
-            res += score_move_node(state, board |  tile_2      , cprob * 0.9) * 0.9;
-            res += score_move_node(state, board | (tile_2 << 1), cprob * 0.1) * 0.1;
+            res += score_move_node(&mut state, board |  tile_2      , cprob * 0.9) * 0.9;
+            res += score_move_node(&mut state, board | (tile_2 << 1), cprob * 0.1) * 0.1;
         }
         tmp >>= 4;
         tile_2 <<= 4;
@@ -336,22 +336,22 @@ fn score_helper(board: u64, table: &[f32]) -> f32{
     table[((board >> 48) & ROW_MASK) as usize] 
 }
 
-fn _score_toplevel_move(state: eval_state, board: u64, mv: u8) -> f32 {
+fn _score_toplevel_move(mut state: &mut eval_state, board: u64, mv: u8) -> f32 {
     let newboard = execute_move(mv, board);
 
     if board == newboard {
         return 0.0;
     }
 
-    score_tilechoose_node(&state, newboard, 1.0) + 0.000001
+    score_tilechoose_node(&mut state, newboard, 1.0) + 0.000001
 }
 
 fn score_toplevel_move(board: u64, mv: u8) -> f32 {
-    let state: eval_state;
+    let mut state = eval_state{maxdepth: 0, curdepth: 0, moves_evaled: 0, cachehits:0, depth_limit:0, trans_table: Trans_Table::new()};
     state.depth_limit = max(3, (count_distinct_tiles(board) - 2) as u32);
 
     let start = SystemTime::now();
-    let res = _score_toplevel_move(state, board, mv);
+    let res = _score_toplevel_move(&mut state, board, mv);
     let diff = match SystemTime::now().duration_since(start) {
         Ok(duration) => duration,
         Err(duration) => {println!("Time error"); duration.duration()}
@@ -369,12 +369,11 @@ fn score_toplevel_move(board: u64, mv: u8) -> f32 {
     res
 }
 
-fn find_best_move(board: u64) -> u8 {
-    let mv: u8;
-    let best: f32 = 0.0;
-    let bestmove: u8 = -1;
+fn find_best_move(mut board: u64) -> u8 {
+    let mut best: f32 = 0.0;
+    let mut bestmove: u8 = 0;
 
-    print_board(board);
+    print_board(&mut board);
     println!("Current scores: heur {}, actual {}", score_heur_board(board), score_board(board));
 
     for mv in 0..4 {
@@ -396,43 +395,40 @@ fn draw_tile() -> u64 {
     }
 }
 
-fn insert_tile_rand(board: u64, tile: u64) -> u64 {
-    let index: u32 = rand::thread_rng().gen_range(0, count_empty(board) as u32);
-    let tmp: u64 = board;
+fn insert_tile_rand(mut board: u64, tile: &mut u64) -> u64 {
+    let index: u32 = rand::thread_rng().gen_range(0, count_empty(&mut board) as u32);
+    let mut tmp: u64 = board;
     loop {
         while (tmp & 0xF) != 0 {
             tmp >>= 4;
-            tile <<= 4;
+            *tile <<= 4;
         }
         if index == 0 { break; }
         --index;
         tmp >>= 4;
-        tile <<= 4;
+        *tile <<= 4;
     }
-    return board | tile;
+    return board | *tile;
 }
 
 fn initial_board() -> u64 {
     let board: u64 = draw_tile() << (4 * rand::thread_rng().gen_range(0, 16));
-    insert_tile_rand(board, draw_tile())
+    insert_tile_rand(board, &mut draw_tile())
 }
 
 fn play_game(get_move: fn(u64) -> u8) {
-    let board: u64 = initial_board();
-    let moveno = 0;
-    let scorepenalty: u32 = 0;
+    let mut board: u64 = initial_board();
+    let mut moveno = 0;
+    let mut scorepenalty: u32 = 0;
 
-    loop {
+'out: loop {
         let mv: u8;
         let newboard: u64;
 
-        for mv in 0..4 {
-            if execute_move(mv, board) != board {
-                break;
+        for i in 0..4 {
+            if execute_move(i, board) != board {
+                break 'out;
             }
-        }
-        if mv == 4 {
-            break;
         }
 
         println!("Mov #{}, current score={}", moveno, score_board(board) - scorepenalty as f32);
@@ -450,12 +446,12 @@ fn play_game(get_move: fn(u64) -> u8) {
             continue;
         }
 
-        let tile: u64 = draw_tile();
+        let mut tile: u64 = draw_tile();
         if tile == 2 {scorepenalty += 4};
-        board = insert_tile_rand(newboard, tile);
+        board = insert_tile_rand(newboard, &mut tile);
     }
 
-    print_board(board);
+    print_board(&mut board);
     println!("Game Over. Score: {}. Highest Tile: {}.", score_board(board) -(scorepenalty as f32), get_max_rank(board));
 }
 
